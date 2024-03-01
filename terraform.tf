7a31837d48de29c458bf821a1c9e4d95731ddaf1
Amie Wei <amie.wei@hashicorp.com>
2024-03-01 16:01:49 -0600

    update provider version to 5.16.2

--- terraform.tf
+++ terraform.tf
@@ -2,7 +2,14 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.16.1"
+      version = "~> 5.16.2"
+    }
+  }
+  cloud {
+    organization = "tf-se-test"
+
+    workspaces {
+      name = "terraform-test-s3"
     }
   }
 
