1eb2901e407cd1e5d06f77882e4e4da28f833bcb
Amie Wei <amie.wei@hashicorp.com>
2025-01-23 10:18:40 -0600

    update variable

--- variables.tf
+++ variables.tf
@@ -1,6 +1,4 @@
 variable "bucket_name" {
   description = "Name of the s3 bucket. Must be unique."
-  default     = null
-  # default     = "test-bucket-for-terraform-test"
-  type = string
+  type        = string
 }
